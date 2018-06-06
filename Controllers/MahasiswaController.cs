6ed53d97116b570259cea61d9350b8a7f02a61ef
Erick Kurniawan <erick.kurniawan@gmail.com>
2018-06-06 17:36:40 +0700

    tambah kota

--- Controllers/MahasiswaController.cs
+++ Controllers/MahasiswaController.cs
@@ -10,10 +10,14 @@ namespace SampleMVC.Controllers
 {
     public class MahasiswaController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(int id,string kota,string telp)
         {
             ViewData["nama"] = "Erick Kurniawan";
             ViewBag.Alamat = "Jl Wahidin 5-25";
+
+            ViewBag.Telp = telp;
+            ViewBag.Kota = kota;
+            ViewBag.Id = id;
 
             return View();
         }
